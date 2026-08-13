dda296fccf1d70ce1be7a8885c79e20d2c613ecf
Ibraheem-Al-hafith <ibraheem.d.alhafiz@gmail.com>
2026-08-13 11:01:20 +0200

    Updating environment

---
+++ main.py
@@ -0,0 +1,11 @@
+from src import visualizer
+from src.data import DataClass
+from src.visualizer import Visualizer
+
+
+def main():
+    dataset = DataClass()
+
+
+if __name__ == "__main__":
+    main()
